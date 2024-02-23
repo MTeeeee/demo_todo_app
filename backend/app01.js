157241637ef5e253edf2e597e970bcef4e386f6f
HelloWorldUsername <112711304+MTeeeee@users.noreply.github.com>
2024-02-23 21:10:40 +0100

    app06.js hat nun Kommentare

--- backend/app01.js
+++ backend/app01.js
@@ -1,19 +1,31 @@
+// Hier haben wir gestartet
+
+
+// In Node.js können wir Javascript schreiben.
+// Die console stellt in diesem Fall unser Terminal dar.
 console.log('Server Online');
 
+// Node.js erlaubt uns mit dem Modul fs mit Dateien auf unserem System zu interagieren.
 const fs = require('fs');
 fs.writeFileSync('hello.txt', 'Hello from Node.js');
 
+
+// Der Code ab hier startet einen Server
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    // Über die Parameter req res können wir die eingehenden requests(req) verwenden
+    // und die response(res) die zurückgesendet wird definieren.
     console.log(req.url)
     console.log(req.method)
     res.end('Hello From Node.js')
 })
 
+// Diese Zeile ist notwendig damit der Server nach dem Starten aktiv bleibt
+// und weiter "hört" ob Anfragen(Requests) eingehen.
 server.listen(3000)
 
-
+// Ab hier sind noch ein paar Zeilen mit denen wir nützliche Prinzipien besprochen hatten.
 // //############################
 // function myFunction(req, res) {
 //     console.log('hello from node')
--- backend/app06.js
+++ backend/app06.js
@@ -1,37 +1,63 @@
 console.log("Server Online");
 
+
+// Module werden geladen:
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs")
 
+// Server wird erstellt und Port festgelegt
 const server = express();
 const PORT = 3000;
 
 //Middleware
+// Middleware sind Funktionen die auf alle eingehenden Requests angewand werden.
+// Requests durchlaufen alle Middleware Funktionen bevor sie zu den Routen gelangen.
 server.use(express.json());
 server.use(cors());
 
+// Ab hier definieren wir unsere Routen und was passieren soll wenn entsprechende Methode darauf eingeht.
+
+// Dies ist die get Schnittstelle von der das Frontend alle aktuellen Todos bekommen soll
 server.get("/todos", (req, res) => {
     console.log("ich bekomme einen get");
+    // Da die Todos in einer json Datei sind müssen wir gebrauch vom fs Modul machen um so diese Datei auszulesen.
     fs.readFile("./todos.json", "utf-8", (err, data) => {
+        // Sollte es einen Fehler beim auslesen geben, wird ein Error zurückgesendet.
         if (err) res.status(500).json({error: 'Datei konnte nicht gelesen werden'});
+        // Die json Datei wird zunächst in data geladen.
+        // Damit wir sie mit Javascript Befehlen bearbeiten können müssen wir sie parsen.
+        // Also aus einer Datei eine Object Variable machen.
         let todos = JSON.parse(data);
+        // Dannach können wir alles als Response ans Frontend zurückschicken.
         res.status(200).json(todos);
       });
 })
 
+// Definiert eine neue Route für den POST-Request auf den Pfad "/todos".
 server.post("/todos", (req, res) => {
+    // Gibt eine Nachricht in der Konsole aus, um anzuzeigen, dass ein POST-Request empfangen wurde.
     console.log("ich bekomme einen post");
+    // Gibt den Inhalt des Request-Bodys in der Konsole aus. Dies sollte das neue Todo-Element sein.
     console.log(req.body);
+    // Speichert den Request-Body (das neue Todo-Element) in einer Variablen.
     let newTodo = req.body;
+
+    // Liest die bestehende Todo-Liste aus der Datei "todos.json".
     fs.readFile("./todos.json", "utf-8", (err, data) => {
+        // Parst die JSON-Daten aus der Datei in ein JavaScript-Objekt.
         let todos = JSON.parse(data);
+        // Fügt das neue Todo-Element am Ende der Todo-Liste hinzu.
         todos.push(newTodo);
+
+        // Schreibt die aktualisierte Todo-Liste zurück in die Datei "todos.json".
         fs.writeFile("./todos.json", JSON.stringify(todos, null, 2), () => {
+            // Sendet eine Antwort mit dem Statuscode 200 (OK) zurück und gibt ein Erfolgsmeldung im JSON-Format zurück.
             res.status(200).json({success: 'Todo wurde abgespeichert'});
       });
     });
 });
+
 
 
 server.listen(PORT, () => {
